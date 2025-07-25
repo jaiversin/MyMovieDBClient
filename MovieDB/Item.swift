eaa08b51cf4f16719a7cfd0feb446dd7f8f10316
Jhon Lopez <jaiversin@gmail.com>
2025-07-24 22:29:56 -0700

    Initial Commit

---
+++ MovieDB/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MovieDB
+//
+//  Created by Jhon Lopez on 7/24/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ MovieDBTests/MovieDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBTests.swift
+//  MovieDBTests
+//
+//  Created by Jhon Lopez on 7/24/25.
+//
+
+import Testing
+@testable import MovieDB
+
+struct MovieDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
